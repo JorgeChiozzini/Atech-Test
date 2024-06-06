8fa1e241710dde6d60f00ba1bf4c501bd916a1b5
Jorge Chiozzini <jchiozzini@gmail.com>
2024-06-06 12:00:24 +0000

    redirecionar html route

--- vue-crud-app/src/App.vue
+++ vue-crud-app/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">ATECH TEST</a>
-      <div class="collapse navbar-collapse">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <router-link to="/" class="nav-link">Home</router-link>
